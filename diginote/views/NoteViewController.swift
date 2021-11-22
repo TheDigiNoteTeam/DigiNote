d8e660f0b08b79e4e53b988f7914f38fd80a3af2
Sayed Jahed <hussini@jahed.me>
2021-11-21 20:45:57 -0500

    user interface created

---
+++ diginote/views/NoteViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NoteViewController.swift
+//  diginote
+//
+//  Created by Sayed Jahed Hussini on 11/16/21.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+   
+    
+    
+//    @IBOutlet var imageView: UIImageView
+//    var imagePicker: ImagePic
+    @IBAction func cameraBtnClicked(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func gallerBtnClicked(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
