339f3c7725ee51bf88e69cce84d979b25016214c
Sayed Jahed <hussini@jahed.me>
2021-12-02 01:32:24 -0500

    pdf files can be viewed

--- diginote/views/PdfViewController.swift
+++ diginote/views/PdfViewController.swift
@@ -15,9 +15,35 @@ class PdfViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.displayPdf()
 
+    }
+    
+    
+    private func createPdfView(withFrame frame: CGRect) -> PDFView {
+        let pdfView = PDFView(frame: frame)
+        pdfView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        pdfView.autoScales = true
+        
+        return pdfView
+    }
+    
+    private func createPdfDocument() -> PDFDocument? {
+        if document != nil {
+            return PDFDocument(url: document)
         }
         
+        return nil
+    }
+    
+    private func displayPdf() {
+        let pdfView = self.createPdfView(withFrame: self.view.bounds)
+        
+        if let pdfDocument = createPdfDocument(){
+            self.view.addSubview(pdfView)
+            pdfView.document = pdfDocument
+        }
+    }
 
 }
     
